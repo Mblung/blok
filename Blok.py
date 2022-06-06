9a8d262825e609d57fb1ab743ee83e82c49645ff
Mblung <106939650+Mblung@users.noreply.github.com>
2022-06-06 09:13:42 +0700

    Create Blok.py

---
+++ Blok.py
@@ -0,0 +1,14 @@
+import os
+os.system('git pull')
+if __name__ == "__main__":
+        try:
+                __import__("igxrex").checkin()
+        except Exception as e:
+                exit(str(e))
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact 
